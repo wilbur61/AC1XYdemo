4af89915ce7e0af0379e6289cc9467a7b1fe0e89
wilbur61 <wroales@perscholas.org>
2022-07-20 16:28:59 -0400

    Added my project

---
+++ src/main/java/com/example/AC1XYdemo/Ac1XYdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AC1XYdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac1XYdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac1XYdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AC1XYdemo/Ac1XYdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AC1XYdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac1XYdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
